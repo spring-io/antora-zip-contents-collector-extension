9f0f4c77623cdfb8ba52f0715a9d012681e6ff1c
Phillip Webb <phil.webb@broadcom.com>
2024-03-21 15:44:44 -0700

    Drop changelog
    Closes gh-5

--- npm/version.js
+++ npm/version.js
@@ -4,26 +4,9 @@ const { promises: fsp } = require('fs')
 const ospath = require('path')
 
 const PROJECT_ROOT_DIR = ospath.join(__dirname, '..')
-const CHANGELOG_FILE = ospath.join(PROJECT_ROOT_DIR, 'CHANGELOG.adoc')
 const PACKAGE_LOCK_FILE = ospath.join(PROJECT_ROOT_DIR, 'package-lock.json')
 const PACKAGES_DIR = ospath.join(PROJECT_ROOT_DIR, 'packages')
 const VERSION = process.env.npm_package_version
-
-function getCurrentDate (now = new Date()) {
-  return new Date(now.getTime() - now.getTimezoneOffset() * 60000)
-}
-
-function updateChangelog (releaseDate) {
-  return fsp.readFile(CHANGELOG_FILE, 'utf8').then((changelog) =>
-    fsp.writeFile(
-      CHANGELOG_FILE,
-      changelog.replace(/^== (?:(Unreleased)|\d.*)$/m, (currentLine, replace) => {
-        const newLine = `== ${VERSION} (${releaseDate})`
-        return replace ? newLine : [newLine, '_No changes since previous release._', currentLine].join('\n\n')
-      })
-    )
-  )
-}
 
 function updatePackageLock () {
   return fsp.readdir(PACKAGES_DIR, { withFileTypes: true }).then((dirents) => {
@@ -64,7 +47,5 @@ function updatePackageLock () {
 }
 
 ;(async () => {
-  const releaseDate = getCurrentDate().toISOString().split('T')[0]
-  await updateChangelog(releaseDate)
   await updatePackageLock()
 })()
